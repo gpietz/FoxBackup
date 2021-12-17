833ebc45cf736e73577c3644cff49e64501b5ad7
gpietz <gpietz@posteo.de>
2021-12-18 00:44:18 +0100

    Initial project checkin.

---
+++ FoxBackup/Worker.cs
@@ -0,0 +1,33 @@
+namespace FoxBackup;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task StartAsync(CancellationToken cancellationToken)
+    {
+        return Task.Run(() =>
+        {
+
+        }, cancellationToken);
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+
+    private void LoadConfiguration()
+    {
+        
+    }
+}
